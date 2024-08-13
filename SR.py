355794a9f4499c15bfa4fca3b90cc05f4be0ec04
Marissha <balabobalaba71@gmail.com>
2024-08-13 15:38:29 +0300

    Add files via upload

---
+++ SR.py
@@ -0,0 +1,12 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+print_params()
+print_params(b=25)
+print_params(c=[1,2,3])
+
+values_list = ['ort', 123, False]
+values_dict = {'a':123, 'b':True, 'c':'ORT'}
+
+print_params(*values_list)
+print_params(**values_dict)
